ce7bf77eb683cbf79a537261eb38784425821c86
nnam099 <khachhanhtltd663@gmail.com>
2026-08-11 21:30:47 +0700

    Backend: Add @Builder to IncidentCategory for tests

--- src/main/java/com/attt/incident/entity/IncidentCategory.java
+++ src/main/java/com/attt/incident/entity/IncidentCategory.java
@@ -2,6 +2,7 @@ package com.attt.incident.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class IncidentCategory {
 
     @Id
